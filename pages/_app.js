afb3f3e3453e8b4b21a1b685eea6955aa94dac7c
manudeep9798 <manudeep9798@gmail.com>
2021-12-21 17:58:25 +0530

    app.js updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
-import { useEffect } from 'react/cjs/react.development'
+
 import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    console.log("loaded");
-  })
+
   return <Component {...pageProps} />
 }
 
